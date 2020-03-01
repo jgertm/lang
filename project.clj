e8c39eec194ef6d707077e5294319018f36b2559
Tim Jäger <jger.tm@gmail.com>
2020-02-29 18:31:26 -0800

    distinguish env

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.blancas/kern "1.1.0"]
                  [org.clojure/core.match "0.3.0"]
@@ -10,6 +10,8 @@
                  [com.gfredericks/debug-repl "0.0.11"]]
   :main ^:skip-aot lang.core
   :target-path "target/%s/"
-  :profiles {:uberjar {:aot :all}} 
+  :profiles {:uberjar {:aot            :all
+                       :resource-paths ["config/uberjar"]}
+             :dev     {:resource-paths ["config/dev"]}} 
   :repl-options {:init-ns          lang.core
                  :nrepl-middleware [com.gfredericks.debug-repl/wrap-debug-repl]})
--- src/lang/utils.clj
+++ src/lang/utils.clj
@@ -1,10 +1,23 @@
 (ns lang.utils
-  (:require [com.gfredericks.debug-repl :refer [break!]]))
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]))
+
+(def dev?
+  (= :dev (:profile (edn/read-string (slurp (io/resource "config.edn"))))))
+
+(when dev?
+  (require '[com.gfredericks.debug-repl :refer [break!]]))
 
 (defmacro undefined
   ;; (throw (Exception. "not implemented yet"))
-  ([] `(break!))
-  ([name] `(break! ~name)))
+  ([]
+   (if dev?
+     `(break!)
+     `(throw (Exception. "not implemented yet"))))
+  ([name]
+   (if dev?
+     `(break! ~name)
+     `(throw (Exception. "not implemented yet")))))
 
 (defn concatv
   [& collections]
