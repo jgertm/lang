e596e0a62ce6ec44ad6488dc0a5d6aa8e75b10a9
Tim Jäger <jger.tm@gmail.com>
2020-09-28 23:59:34 -0700

    indexed tests
    this leads to more concise output on failure

--- test/lang/type_checker_test.clj
+++ test/lang/type_checker_test.clj
@@ -2,73 +2,65 @@
   (:require [lang.test-prelude :refer :all]
             [lang.module :as module]))
 
-(fact "atoms typecheck"
-  (->
-    (run :type-checker
-      (defmodule lang.type-checker-test.atom-definitions
-        (:skip-implicits))
-      (def foo 2)
-      (def bar "bar")
-      (def baz true)
-      (def quux nil))
-    :values)
-  => (matches
-       {{:reference :constant
-         :name      "foo"
-         :in        {:reference :module
-                     :name      ["lang" "type-checker-test" "atom-definitions"]}}
-        [{:ast/type :named
-          :name     {:reference :type
-                     :name      "Integer"
-                     :in        {:reference :module :name ["lang" "builtin"]}}}
-         :principal]
-        {:reference :constant
-         :name      "bar"
-         :in        {:reference :module
-                     :name      ["lang" "type-checker-test" "atom-definitions"]}}
-        [{:name {:name "String"}} :principal]
-        {:reference :constant
-         :name      "baz"
-         :in        {:reference :module
-                     :name      ["lang" "type-checker-test" "atom-definitions"]}}
-        [{:name {:name "Bool"}} :principal]
-        {:reference :constant
-         :name      "quux"
-         :in        {:reference :module
-                     :name      ["lang" "type-checker-test" "atom-definitions"]}}
-        [{:name {:name "Unit"}} :principal]}))
+(facts "atoms typecheck"
+  (let [{:strs [foo bar baz quux]}
+        (->>
+          (run :type-checker
+            (defmodule lang.type-checker-test.atom-definitions
+              (:skip-implicits))
+            (def foo 2)
+            (def bar "bar")
+            (def baz true)
+            (def quux nil))
+          :values
+          (map (fn [[k v]] [(:name k) v]))
+          (into {}))]
+
+    (fact "foo" foo =>
+      (matches [{:ast/type :named
+                 :name     {:reference :type
+                            :name      "Integer"
+                            :in        {:reference :module :name ["lang" "builtin"]}}}
+                :principal]))
+
+    (fact "bar" bar =>
+      (matches [{:name {:name "String"}} :principal]))
+
+    (fact "baz" baz =>
+      (matches [{:name {:name "Bool"}} :principal]))
+
+    (fact "quux" quux =>
+      (matches [{:name {:name "Unit"}} :principal]))))
 
 
-(fact "functions typecheck"
-  (->
-    (run :type-checker
-      (defmodule lang.parser-test.lambda-definitions
-        (:skip-implicits))
-      (defn id [x] x)
-      (defn const [x y] x))
-    :values)
-  => (matches
-       {{:reference :constant
-         :name      "id"
-         :in        {:reference :module
-                     :name      ["lang" "parser-test" "lambda-definitions"]}}
-        [{:ast/type :forall
-          :variable {:ast/type :universal-variable}
-          :body     {:ast/type :function
-                     :domain   {:ast/type :universal-variable}
-                     :return   {:ast/type :universal-variable}}}
-         :principal]
-        {:reference :constant
-         :name      "const"
-         :in        {:reference :module
-                     :name      ["lang" "parser-test" "lambda-definitions"]}}
-        [{:ast/type :forall
-          :variable {:ast/type :universal-variable}
-          :body     {:ast/type :forall
-                     :variable {:ast/type :universal-variable}
-                     :body     {:ast/type :function
-                                :domain   {:ast/type :universal-variable}
-                                :return   {:ast/type :function
-                                           :domain   {:ast/type :universal-variable}
-                                           :return   {:ast/type :universal-variable}}}}}
-         :principal]}))
+(facts "functions typecheck"
+  (let [{:strs [id const]}
+        (->>
+          (run :type-checker
+            (defmodule lang.parser-test.lambda-definitions
+              (:skip-implicits))
+            (defn id [x] x)
+            (defn const [x y] x))
+          :values
+          (map (fn [[k v]] [(:name k) v]))
+          (into {}))]
+
+    (fact "id" id =>
+      (matches [{:ast/type :forall
+                 :variable {:ast/type :universal-variable}
+                 :body     {:ast/type :function
+                            :domain   {:ast/type :universal-variable}
+                            :return   {:ast/type :universal-variable}}}
+                :principal]))
+
+    (fact "const" const =>
+      (matches [{:ast/type :forall
+                 :variable {:ast/type :universal-variable}
+                 :body     {:ast/type :forall
+                            :variable {:ast/type :universal-variable}
+                            :body     {:ast/type :function
+                                       :domain   {:ast/type :universal-variable}
+                                       :return   {:ast/type :function
+                                                  :domain   {:ast/type :universal-variable}
+                                                  :return   {:ast/type :universal-variable}}}}}
+                :principal]))))
