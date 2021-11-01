3836111c8bc9e6d9920b11fc66edd3b3d9a58801
Tim Jäger <jger.tm@gmail.com>
2021-11-01 00:07:28 -0700

    Cached queries

--- src/lang/parser.clj
+++ src/lang/parser.clj
@@ -2,6 +2,7 @@
   (:require [blancas.kern.core :as kern]
             [clojure.core.match :refer [match]]
             [lang.parser.definition :as definition]
+            [lang.state :refer [definput defquery]]
             [taoensso.timbre :as log]))
 
 (def ^:private file
@@ -17,3 +18,34 @@
     (if (:ok result)
       (:value result)
       (throw (ex-info "Parsing error" (:error result))))))
+
+(defquery source [key]
+  (log/debug "reading file" key)
+  (match key
+         [:file path]
+         (slurp path)))
+
+(defquery ast [key]
+   (log/debug "parsing ast" key)
+   (let [text (source key)
+         result (kern/parse file text)]
+     (if (:ok result)
+       (:value result)
+       (throw (ex-info "Parsing error" (:error result))))))
+
+(defquery definitions [key]
+  (log/debug "extracting definitions" key)
+  (->> key
+       ast
+       :definitions
+       (map (juxt :ast/definition (comp :name :name)))))
+
+(comment
+
+  (definitions [:file "std/lang/string.lang"])
+
+  (ast [:file "std/lang/string.lang"])
+
+  (definitions [:file "std/lang/option.lang"])
+
+  )
--- src/lang/state.clj
+++ src/lang/state.clj
@@ -1,4 +1,6 @@
-(ns lang.state)
+(ns lang.state
+  (:require [clojure.core.cache.wrapped :as cache]
+            [taoensso.timbre :as log]))
 
 (def state (atom {}))
 
@@ -13,3 +15,34 @@
        (get val step)))
    @state
    path))
+
+(def cache (cache/lru-cache-factory {}))
+
+(def ^:dynamic *queries* nil)
+(def ^:dynamic *inputs* nil)
+
+(defmacro definput [name params & body]
+  `(defn ~name
+     ~params
+     ~@body))
+
+(defmacro defquery [name params & body]
+  `(defn ~name
+     ~params
+     (let [subqueries# (atom {})
+           key#
+           ~{:query (resolve name)
+             :args  (mapv #(vector (list `quote %) %) params)}]
+       (:result (cache/lookup-or-miss cache key#
+                              (fn [key#]
+                                (let [record#
+                                      {:result     (binding [*queries* subqueries#] ~@body)
+                                       :subqueries (->> @subqueries#
+                                                        (map (fn [[k# v#]] [k# (:result v#)]))
+                                                        (into {}))}]
+                                  (some-> *queries* (swap! assoc key# record#))
+                                  record#)))))))
+
+(macroexpand-1 '(defquery foo [bar quux] :baz))
+
+@cache
