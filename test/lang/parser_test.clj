0ea9a3fec26021a914c6e9438e2a7e2b5ed46b4a
Eric Loren <eric.r.loren@gmail.com>
2021-11-16 18:05:31 -0500

    simpler test

--- test/lang/parser_test.clj
+++ test/lang/parser_test.clj
@@ -52,13 +52,15 @@
                        :body           {:ast/term :atom :atom {:atom :integer :value "2"}}}
                       {:body {:atom {:atom :string :value "bar"}}}
                       {:body {:atom {:atom :boolean :value true}}}
-                      {:body {:atom {:atom :unit :value nil}}}]}
+                      {:body {:atom {:atom :unit :value nil}}}
+                      {:body {:atom {:atom :integer :value "55"}}}]}
        (run :parser
          (defmodule lang.parser-test.atom-definitions)
          (def foo 2)
          (def bar "bar")
          (def baz true)
-         (def quux nil)))))
+         (def quux nil)
+         "(def qaz 5_5)"))))
 
 (deftest parse-constructors
   (is (match?
@@ -158,22 +160,3 @@
            (true? :$ (-> T Bool)))
          (definstance (Veracious Bool)
            (true? [bool] bool))))))
-
-(deftest underscore_numbers
-  (is
-   (match?
-    {:ast/definition :module
-     :name {:reference :module, :name ["lang" "ints-module"]}
-     :skip-implicits nil
-     :imports nil
-     :definitions [{:ast/definition :constant
-                    :name {:reference :constant, :name "a"}
-                    :body {:ast/term :atom, :atom {:atom :integer, :value "11"}}}
-                   {:ast/definition :constant
-                    :name {:reference :constant :name "b"}
-                    :body {:ast/term :atom, :atom {:atom :integer, :value "11"}}}]}
-    (run :parser
-         "(defmodule lang.ints-module)"
-         "(def a 1_1)"
-         "(def b 11)"
-         "(def c 1_1.2_2)"))))
